07009abfd6e2c21c3921a836398a54fcc8342d09
Ralph Ramos <ralphmtk@gmail.com>
2024-11-08 09:36:50 -0300

    Funcionando

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,18 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const cardContainer = document.querySelector(".card-container");
-  const images = ["./assets/slide/slide01.jpg", "./assets/slide/slide02.jpg"];
+  const images = ["../assets/slide/slide01.jpg", "../assets/slide/slide02.jpg"];
   let currentSlide = 0;
 
   function startSlideShow() {
-    // Exibe a primeira imagem imediatamente
     cardContainer.style.backgroundImage = `url('${images[currentSlide]}')`;
 
-    // Inicia o intervalo de troca de imagens após exibir a primeira imagem
     setInterval(() => {
       currentSlide = (currentSlide + 1) % images.length;
       cardContainer.style.backgroundImage = `url('${images[currentSlide]}')`;
-      console.log(`Mostrando imagem: ${images[currentSlide]}`);
-    }, 6000); // alterna a cada 3 segundos
+    }, 6000); // alterna a cada 6 segundos
   }
 
   startSlideShow();
